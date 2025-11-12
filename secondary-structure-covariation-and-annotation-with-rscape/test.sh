f46dd7553717f525a54e3e21c0db98d1b708dc42
mmagnus <mag_dex@o2.pl>
2025-11-12 14:38:09 -0500

    rscape update

--- secondary-structure-covariation-and-annotation-with-rscape/test.sh
+++ secondary-structure-covariation-and-annotation-with-rscape/test.sh
@@ -1,8 +1,9 @@
-#R-scape --rna --outprep 1ehz_RF00005.sto > 1ehz_RF00005_rscape.txt
-#R-scape --rna --pdb 1ehz_A.pdb --outprep 1ehz_RF00005.sto > 1ehz_RF00005_rscape_pdb.txt
-#R-scape --rna --pdb 1ehz_A_rpr.pdb --outprep 1ehz_RF00005.sto > 1ehz_rpr_RF00005_rscape_pdb.txt
+R-scape --rna --outprep 1ehz_RF00005.sto > 1ehz_RF00005_rscape.txt
+R-scape --rna --pdb 1ehz_A.pdb --outprep 1ehz_RF00005.sto > 1ehz_RF00005_rscape_pdb.txt
+R-scape --rna --pdb 1ehz_A_rpr.pdb --outprep 1ehz_RF00005.sto > 1ehz_rpr_RF00005_rscape_pdb.txt
 # # R-scape 2.0.4.i (July 2024) ^^^^^^
-#/Users/magnus/work/opt/r-scape/rscape_v2.5.9.a/bin/R-scape --rna --pdb 1EHZ_fPDB.pdb --outprep 1ehz_RF00005.sto > 1EHZ_fPDB_RF00005_rscape_pdb.txt
-#/Users/magnus/work/opt/r-scape/rscape_v2.5.9.a/bin/R-scape --rna --pdb 1EHZ_fPDB_rpr.pdb --outprep 1ehz_RF00005.sto > 1EHZ_fPDB_rpr_RF00005_rscape_pdb.txt
-#/Users/magnus/work/opt/r-scape/rscape_v2.5.9.a/bin/R-scape --rna --pdb 5BTP.pdb seq+RF01750_seed_1.cacofold.sto
-/Users/magnus/work/opt/r-scape/rscape_v2.6.4/bin/R-scape --rna --pdb 5BTP.pdb seq+RF01750_seed_1.cacofold.sto #> 5BTP_RF01750_rscape264_pdb.txt
+/Users/magnus/work/opt/r-scape/rscape_v2.5.9.a/bin/R-scape --rna --pdb 1EHZ_fPDB.pdb --outprep 1ehz_RF00005.sto      > 1EHZ_fPDB_RF00005_rscape_pdb.txt
+/Users/magnus/work/opt/r-scape/rscape_v2.5.9.a/bin/R-scape --rna --pdb 1EHZ_fPDB_rpr.pdb --outprep 1ehz_RF00005.sto  > 1EHZ_fPDB_rpr_RF00005_rscape_pdb.txt
+
+/Users/magnus/work/opt/r-scape/rscape_v2.5.9.a/bin/R-scape --rna --pdb 5BTP.pdb seq+RF01750_seed_1.cacofold.sto      > 5BTP_RF01750_rscape259_pdb.txt
+/Users/magnus/work/opt/r-scape/rscape_v2.6.4/bin/R-scape --rna --pdb 5BTP.pdb seq+RF01750_seed_1.cacofold.sto        > 5BTP_RF01750_rscape264_pdb.txt
